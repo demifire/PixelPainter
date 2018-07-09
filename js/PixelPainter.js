2474ae06ed67007b8f40d4803137a565cc0f5f70
Chaz Kondo <Chaz@2ppgn4x.local>
2018-07-08 17:33:06 -1000

    Doing stuff again

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,18 +1,5 @@
-const pixelDiv = document.createElement('div');
-document.body.appendChild(pixelDiv);
-
 var lastClicked;
-var grid = clickableGrid(12,12,function(el,row,col,i){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
-
-    // el.className='black';
-    // if (lastClicked)
-    //         lastClicked.className='';
-    //         lastClicked = el;
-});
+var grid = clickableGrid(30,30);
 
 document.body.appendChild(grid);
 var color = null;
@@ -28,7 +15,7 @@ function clickableGrid( rows, cols){
             cell.innerHTML = '';
             cell.addEventListener('click', function(){
                 if(color === null){
-                this.style.backgroundColor = 'pink';
+                this.style.backgroundColor = 'indianred';
             }else{
                 this.style.backgroundColor = color;
             }
@@ -39,61 +26,44 @@ function clickableGrid( rows, cols){
     return grid;
 }
 
-var colorDiv = document.createElement('div');
-colorDiv.className = 'colorDivClass';
-colorDiv.innerHTML = 'Black';
-colorDiv.id = 'clickedEm';
+
+var colorDiv = document.createElement("table");
+colorDiv.className = "colorDiv";
 document.body.appendChild(colorDiv);
 
-colorDiv.addEventListener('click',function(){
-    color = 'black';
-    return color;
-})
+for (var i=0; i<9; i++) {
+    var makeColorRow = document.createElement("tr");
+    makeColorRow.className = "colorRow";
+    colorDiv.appendChild(makeColorRow);
+}
+var colorRow = document.getElementsByClassName("colorRow");
 
-var colorDiv2 = document.createElement('div');
-colorDiv2.className = 'colorDivClass';
-colorDiv2.innerHTML = 'Green';
-colorDiv2.id = 'clickedEm';
-document.body.appendChild(colorDiv2);
-
-colorDiv2.addEventListener('click',function(){
-    color = 'green';
-    return color;
-})
-
-// colorDiv.addEventListener('click', changeColor);
-
-// var something = document.createElement('div');
-// something.Id = 'clickedEm';
-// something.innerHTML = 'something';
-// document.appendChild(something);
-
-function clickableGrid2( rows, cols){
-    var i=0;
-    var grid = document.createElement('table');
-    grid.className = 'grid';
-    for (var r=0;r<rows;++r){
-        var tr = grid.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
-            var cell = tr.appendChild(document.createElement('td'));
-            cell.innerHTML = '';
-            cell.addEventListener('click', function(arr){
-                for(var l=0; l<arr.length;l++){
-                    this.style.backgroundColor = arr[l]
-                }
-        }
-        );
-        }
+for (var i=0; i<colorRow.length; i++) {
+    for (var j=0; j<8; j++) {
+        var makeColorSquare = document.createElement("td");
+        makeColorSquare.className = "colorSquare";
+        makeColorSquare.addEventListener("click", selectColor);
+        colorRow[i].appendChild(makeColorSquare);
     }
-    return grid;
 }
 
-var grid2 = clickableGrid2(2,2, function(el,row,col,i){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
+var colorSquare = document.getElementsByClassName("colorSquare");
+colorSquare[0].style.border = "1px solid yellow"
 
-});
+function selectColor() {
+    for (var i=0; i<colorSquare.length; i++) {
+        if (colorSquare[i].style.border === "1px solid yellow") {
+            colorSquare[i].style.border = "";
+        }
+    }
+    color = this.style.backgroundColor;
+    console.log(color);
+    this.style.border = "1px solid yellow";
+}
 
-document.body.appendChild(grid2);
+var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"]
+
+for (var i=0; i<colorArr.length; i++) {
+    colorSquare[i].style.backgroundColor = colorArr[i];
+}
+
