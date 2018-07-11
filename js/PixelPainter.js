bc913571113e20a48021ce771f379d1e72fd9ab8
Chaz Kondo <Chaz@2ppgn4x.local>
2018-07-11 13:58:37 -1000

    Updated my file with download image feature

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -112,6 +112,9 @@ colorDiv.className = "colorDiv";
 pixelDiv.appendChild(colorDiv);
 
 var rowsAndColumns = 20;
+var previousRowsAndColumns = 0;
+var previousGridCells = rowsAndColumns*rowsAndColumns;
+var initialGrid = 0;
 var grid = clickableGrid(rowsAndColumns);
 
 pixelDiv.appendChild(grid);
@@ -125,6 +128,7 @@ function clickableGrid(number){
         for (var c=0;c<number;++c){
             var cell = tr.appendChild(document.createElement('td'));
             cell.innerHTML = '';
+            cell.style.backgroundColor = 'white';
             cell.className = 'cells'
             cell.addEventListener('click', function(){
                 if(color === null){
@@ -169,7 +173,7 @@ function selectColor() {
     this.style.border = "1px solid yellow";
 }
 
-var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"]
+var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"];
 
 for (var i=0; i<colorArr.length; i++) {
     colorSquare[i].style.backgroundColor = colorArr[i];
@@ -185,7 +189,7 @@ clear.addEventListener('click', makeGridEmpty)
 function makeGridEmpty(){
     var allSquares = document.querySelectorAll('.cells');
     for(var i=0;i<allSquares.length;i++){
-        allSquares[i].style.backgroundColor = "";
+        allSquares[i].style.backgroundColor = "white";
     }
 }
 
@@ -202,7 +206,7 @@ function makeColorWhite(){
             colorSquare[i].style.border = "";
         }
     }
-    color = '';
+    color = 'white';
     colorSquare[71].style.border = "1px solid yellow"
 }
 
@@ -256,26 +260,120 @@ changeGridButton.addEventListener('click', changeGridDisplay)
 
 function changeGridDisplay(){
     var gridInputValue = document.getElementById('changeGridInput').value;
-    if(gridInputValue > 0 && gridInputValue <101){
-        if (confirm('Are you sure you want to change grid? Your work will not be saved.')) {
+    if(gridInputValue > 0 && gridInputValue <99){
+        if(confirm('Are you sure you want to change grid? Your work will not be saved.')) {
             grid.style.display = 'none';
+            previousRowsAndColumns = rowsAndColumns;
             rowsAndColumns = gridInputValue;
             grid = clickableGrid(rowsAndColumns);
             pixelDiv.appendChild(grid);
+            previousGridCells += rowsAndColumns*rowsAndColumns;
+            initialGrid += previousRowsAndColumns*previousRowsAndColumns;
         } else {
             // Do nothing!
         }
-    }else{
+    } else if(gridInputValue == 99){
+        if(confirm('Are you sure you want to change grid? Your work will not be saved.')) {
+            grid.style.display = 'none';
+            previousRowsAndColumns = rowsAndColumns;
+            rowsAndColumns = gridInputValue;
+            grid = clickableGrid(rowsAndColumns);
+            pixelDiv.appendChild(grid);
+            convertImage(previousGridCells);
+            previousGridCells += rowsAndColumns*rowsAndColumns;
+            initialGrid += previousRowsAndColumns*previousRowsAndColumns;
+        } else {
+            // Do nothing!
+        }
+    } else {
         alert('Invalid entry')
     }
 }
 
 
+// var canvas = document.createElement("canvas");
+// var ctx = canvas.getContext("2d");
+// canvas.width  = 20;
+// canvas.height = 20;
+// document.body.appendChild(canvas);
+
+// var redArr = [];
+// var greenArr = [];
+// var blueArr = [];
+// var alphaArr = [];
+
+// var logoImg = new Image();
+// logoImg.onload = function () {
+//     ctx.drawImage(logoImg, 0, 0);
+//     var pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+// // console.log(pixels);
+//     var pixelArr = pixels.data;
+// console.log(pixels.data);
+
+//     for(var i=0;i<pixelArr.length;i+=4){
+//         redArr.push(pixelArr[i+0]);
+//         greenArr.push(pixelArr[i+1]);
+//         blueArr.push(pixelArr[i+2]);
+//         alphaArr.push(pixelArr[i+3]);
+
+
+//     }
+
+//     function doSomething(){
+//     var allSquares = document.querySelectorAll('.cells');
+//     for(var i=0;i<allSquares.length;i++){
+//             allSquares[i].style.backgroundColor='rgb(' + redArr[i] + ',' + greenArr[i] + ',' + blueArr[i] + ',' + alphaArr[i] + ')';
+//     }
+// }
+// }
+// logoImg.src = "images/ok.png";
+
+
+// Sean pic begin
+// var canvas = document.createElement("canvas");
+// var ctx = canvas.getContext("2d");
+// canvas.width  = 100;
+// canvas.height = 100;
+// // document.body.appendChild(canvas);
+
+// var redArr = [];
+// var greenArr = [];
+// var blueArr = [];
+// var alphaArr = [];
+
+// var logoImg = new Image();
+// logoImg.onload = function () {
+//     ctx.drawImage(logoImg, 0, 0);
+//     var pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+//     var pixelArr = pixels.data;
+
+//     for(var i=0;i<pixelArr.length;i+=4){
+//         redArr.push(pixelArr[i+0]);
+//         greenArr.push(pixelArr[i+1]);
+//         blueArr.push(pixelArr[i+2]);
+//         alphaArr.push(pixelArr[i+3]);
+
+
+//     }
+// }
+
+// logoImg.src = "images/test2.jpg";
+
+// function convertImage(shift){
+//     var allSquares = document.querySelectorAll('.cells');
+//     for(var i=shift;i<allSquares.length;i++){
+//         allSquares[i].style.backgroundColor='rgb(' + redArr[i-shift] + ',' + greenArr[i-shift] + ',' + blueArr[i-shift] + ',' + alphaArr[i-shift] + ')';
+//     }
+// }
+// Sean pic End
+
+
+
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width  = 20;
-canvas.height = 20;
-document.body.appendChild(canvas);
+canvas.width  = 99;
+canvas.height = 99;
+// document.body.appendChild(canvas);
 
 var redArr = [];
 var greenArr = [];
@@ -286,9 +384,7 @@ var logoImg = new Image();
 logoImg.onload = function () {
     ctx.drawImage(logoImg, 0, 0);
     var pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
-// console.log(pixels);
     var pixelArr = pixels.data;
-console.log(pixels.data);
 
     for(var i=0;i<pixelArr.length;i+=4){
         redArr.push(pixelArr[i+0]);
@@ -298,21 +394,77 @@ console.log(pixels.data);
 
 
     }
-    
-    var doSomethingButton = document.createElement('button');
-    doSomethingButton.innerHTML = "Do Something";
-    doSomethingButton.id = 'doSomethingButton';
-    colorDiv.appendChild(doSomethingButton);
+}
 
-    doSomethingButton.addEventListener('click', doSomething)
+logoImg.src = "images/bob.jpg";
 
-    function doSomething(){
+function convertImage(shift){
     var allSquares = document.querySelectorAll('.cells');
-    for(var i=0;i<allSquares.length;i++){
-            allSquares[i].style.backgroundColor='rgb(' + redArr[i] + ',' + greenArr[i] + ',' + blueArr[i] + ',' + alphaArr[i] + ')';
+    for(var i=shift;i<allSquares.length;i++){
+        allSquares[i].style.backgroundColor='rgb(' + redArr[i-shift] + ',' + greenArr[i-shift] + ',' + blueArr[i-shift] + ',' + alphaArr[i-shift] + ')';
     }
 }
-}
-logoImg.src = "images/ok.png";
+
+/* REGISTER DOWNLOAD HANDLER */
+/* Only convert the canvas to Data URL when the user clicks. 
+   This saves RAM and CPU ressources in case this feature is not required. */
+var downloadImagePixelArr = [];
+var downloadImageArr = [];
+function dlCanvas() {
+
+    var allSquares = document.querySelectorAll('.cells');
+    downloadImageArr = [];
+    for(var i=initialGrid;i<allSquares.length;i++){
+        downloadImagePixelArr.push(allSquares[i].style.backgroundColor); 
+    }
+
+    while(downloadImagePixelArr.length>0){
+        var rowSegment = downloadImagePixelArr.splice(0,rowsAndColumns);
+        downloadImageArr.push(rowSegment)
+    }
+
+    console.log(initialGrid);
+    console.log(downloadImagePixelArr);
+    console.log(downloadImageArr);
+    
+    var downloadCanvas = document.createElement("canvas"), // Select our canvas element
+    ctx2 = downloadCanvas.getContext("2d"), // Save the context we're going to use
+    width = downloadImageArr[0].length, // Get the width
+    height = downloadImageArr.length, // Get the height
+    scale = 10; // Scales the whole image by this amount
+
+    downloadCanvas.width = width * scale; // Set the canvas width
+    downloadCanvas.height = height * scale; // Set the canvas height
+    downloadCanvas.id = 'downloadCanvas';
+
+    // Loop through each color and draw that section
+    for(var row = 0; row < height; row++) {
+        for(var col = 0; col < width; col++) {
+            ctx2.fillStyle = downloadImageArr[row][col]; // Set the color to the one specified
+            ctx2.fillRect(col * scale, row * scale, scale, scale); // Actually draw the rectangle
+        }
+    }   
+
+    // document.body.appendChild(downloadCanvas);
+
+    var dt = downloadCanvas.toDataURL('image/png');
+    /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+    dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+    /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+    dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+    this.href = dt;
+};
+
+var downloadButtonChild = document.createElement('button');
+downloadButtonChild.innerHTML = 'Download Canvas';
+downloadButtonChild.id = 'downloadImageButton';
+var downloadButton = document.createElement('a');
+downloadButton.download = "Canvas.png";
+downloadButton.href = "#";
+downloadButton.addEventListener('click', dlCanvas, false);
+colorDiv.appendChild(downloadButton);
+downloadButton.appendChild(downloadButtonChild);
 
 
